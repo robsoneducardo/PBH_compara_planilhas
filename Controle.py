a0181da0990d46316c676a83cd8536e5adabae90
robson.devnix <robson.devnix@gmail.com>
2022-03-02 08:30:04 -0300

    Definido formato de célula estilo Excel

---
+++ Controle.py
@@ -0,0 +1,81 @@
+import xlrd
+import typing
+import re
+# from termcolor import colored
+
+class Controle:
+
+    def __init__(self) -> None:
+        self.__planilha_esocial = None
+        self.__planilha_candidata = None
+        self.__relatorio:typing.IO = None
+    
+
+    def set_planilha_esocial(self, nome_pasta_esocial:str) -> None:
+        try:
+            workbook_esocial = xlrd.open_workbook(nome_pasta_esocial)
+            self.__planilha_esocial = workbook_esocial.sheet_by_name('Sheet1')
+        except:
+            # print("\n\n--- olha o outro erro aí, amigo --- \n\n")
+            raise Exception("Falha ao abrir planilha padrão (eSocial).")
+        
+
+    def set_planilha_candidata(self, nome_pasta_candidata: str) -> None:
+        try:
+            workbook_candidata = xlrd.open_workbook(nome_pasta_candidata) 
+            self.__planilha_candidata = workbook_candidata.sheet_by_name('Mapeamento Final')
+        except:
+            raise Exception("Falha ao abrir planilha candidata.")
+
+
+    def set_relatorio(self, relatorio: typing.IO) -> None:
+        self.__relatorio = relatorio
+
+    def escreve_relatorio(self):
+        total_de_linhas = min([len(list(self.__planilha_esocial.get_rows())),
+                len(list(self.__planilha_candidata.get_rows()))])
+        linhas_que_importam = list(range(1, total_de_linhas))
+        colunas_que_importam = list(range(0,9))
+        espacosExtrasPorCelula = 5
+
+        colunas = "ABCDEFGHIJKLMNOPQRSTUWXYZ"
+
+        def get_string_celula(worksheet_esocial, worksheet_arterh, i:int, j:int, ordinal:int):
+            caracteresExibidosPorCelula = [5, 15, 15, 2, 5, 5, 5, 5, 2000]
+
+            conteudo_esocial = ""
+            conteudo_arterh = ""
+
+            try:
+                conteudo_esocial = int(worksheet_esocial.cell(i, j).value)
+            except:
+                conteudo_esocial = str(worksheet_esocial.cell(i, j).value)
+            finally:
+                conteudo_esocial = str(conteudo_esocial)
+
+            conteudo_esocial = re.sub(r"\W", "", conteudo_esocial)[0:caracteresExibidosPorCelula[ordinal]]\
+                .ljust(caracteresExibidosPorCelula[ordinal]+espacosExtrasPorCelula, ' ')
+
+            try:
+                conteudo_arterh = int(worksheet_arterh.cell(i, j).value)
+            except:
+                conteudo_arterh = str(worksheet_arterh.cell(i, j).value)
+            finally:
+                conteudo_arterh = str(conteudo_arterh)
+
+            conteudo_arterh = re.sub(r"\W", "", conteudo_arterh)[0:caracteresExibidosPorCelula[ordinal]]\
+                .ljust(caracteresExibidosPorCelula[ordinal]+espacosExtrasPorCelula, ' ')
+
+            return str(conteudo_esocial), str(conteudo_arterh)
+
+        for i in linhas_que_importam:
+            for ordinal, j in enumerate(colunas_que_importam):
+                conteudo_esocial, conteudo_arterh = get_string_celula(self.__planilha_esocial,
+                        self.__planilha_candidata, i, j, ordinal)
+
+                if conteudo_esocial != conteudo_arterh:
+                    #conteudo_esocial = colored('\033[4m' + conteudo_esocial + '\033[0m', 'red')
+
+                    self.__relatorio.write(f"{colunas[j]}{i+1}:\n")
+                    self.__relatorio.write(f"{conteudo_esocial}\n")
+                    self.__relatorio.write(f"{conteudo_arterh}\n\n")
--- MainForm.py
+++ MainForm.py
@@ -5,67 +5,84 @@ from tkinter import ttk
 from tkinter import filedialog as fd
 from typing import IO
 
+from Controle import Controle
+
 class MainForm (Tk):
     def __init__(self) -> None:
         super().__init__()
         self.title("Diferenciação de Planilhas")
-        self.geometry("400x100")
+        self.geometry("600x100")
         self.resizable(False, False)
+        
+        self.__controle: Controle = Controle()
+
+        self.__pasta_padrao: str = ""
+        self.__pasta_candidata: str = ""
+        self.__relatorio: str = ""
 
 
-        self.pasta_padrao: str = ""
         self.lbl_pasta_padrao: ttk.Label = ttk.Label(self, text= "Pasta Padrão: ")
         
         def lbl_pasta_padrao_click() -> None:
-            self.pasta_padrao = fd.askopenfilename(filetypes=(('planilha xlsx', '*.xlsx'),))
-            self.lbl_pasta_padrao.config(text="Pasta Padrão: " + self.pasta_padrao)
-            if valida(self.pasta_padrao, self.pasta_candidata, self.relatorio):
-                executa(self.lbl_status)
+            self.__pasta_padrao = fd.askopenfilename(filetypes=(('planilha xls', '*.xls'),))
+            try:
+                self.__controle.set_planilha_esocial(self.__pasta_padrao)
+                self.lbl_pasta_padrao.config(text="Pasta Padrão: " + self.__pasta_padrao)
+                if valida(self.__pasta_padrao, self.__pasta_candidata, self.__relatorio):
+                    self.lbl_status.config(text="Gravando relatório...")
+                    self.__controle.escreve_relatorio()
+                    self.lbl_status.config(text="Relatório gravado.")
+            except Exception as e:
+                self.lbl_status.config(text="Erro: " + str(e))
 
         self.lbl_pasta_padrao.grid()
         self.lbl_pasta_padrao.bind("<Button-1>", lambda e: lbl_pasta_padrao_click())
 
 
-        self.pasta_candidata: str = ""
         self.lbl_pasta_candidata: ttk.Label = ttk.Label(self, text= "Pasta Candidata: ")
         
         def lbl_pasta_candidata_click() -> None:
-            self.pasta_candidata = fd.askopenfilename(filetypes=(('planilha xlsx', '*.xlsx'),))
-            self.lbl_pasta_candidata.config(text="Pasta Candidata: " + self.pasta_candidata)
-            if valida(self.pasta_padrao, self.pasta_candidata, self.relatorio):
-                executa(self.lbl_status)
+            self.__pasta_candidata = fd.askopenfilename(filetypes=(('planilha xls', '*.xls'),))
+            try:
+                self.__controle.set_planilha_candidata(self.__pasta_candidata)
+                self.lbl_pasta_candidata.config(text="Pasta Candidata: " + self.__pasta_candidata)
+                if valida(self.__pasta_padrao, self.__pasta_candidata, self.__relatorio):
+                    self.lbl_status.config(text="Gravando relatório...")
+                    self.__controle.escreve_relatorio()
+                    self.lbl_status.config(text="Relatório gravado.")
+                else:
+                    self.lbl_status.config(text="Não validou.")
+            except Exception as e:
+                self.lbl_status.config(text="Erro: " + str(e))
 
         self.lbl_pasta_candidata.grid()
         self.lbl_pasta_candidata.bind("<Button-1>", lambda e: lbl_pasta_candidata_click())
 
 
-        self.relatorio: IO
         self.lbl_relatorio: ttk.Label = ttk.Label(self, text= "Relatório: ")
         
         def lbl_relatorio_click() -> None:
             try:
-                with fd.asksaveasfile(filetypes=(('text file', '*.txt'),)) as self.relatorio:
-                    self.lbl_relatorio.config(text="Relatorio: " + self.relatorio.name)
-                    if valida(self.pasta_padrao, self.pasta_candidata, self.relatorio):
-                        executa(self.lbl_status)
-            finally:
-                pass
+                with fd.asksaveasfile(filetypes=(('text file', '*.txt'),)) as relatorio:
+                    self.__controle.set_relatorio(relatorio)
+                    self.lbl_relatorio.config(text="Relatorio: " + relatorio.name)
+                    self.__relatorio = relatorio.name
+                    if valida(self.__pasta_padrao, self.__pasta_candidata, self.__relatorio):
+                        self.lbl_status.config(text="Gravando relatório...")
+                        self.__controle.escreve_relatorio()
+                        self.lbl_status.config(text="Relatório gravado.")
+            except Exception as e:
+                self.lbl_status.config(text="Erro: " + str(e))
 
         self.lbl_relatorio.grid()
         self.lbl_relatorio.bind("<Button-1>", lambda e: lbl_relatorio_click())
 
 
-        self.status = ""
         self.lbl_status: ttk.Label = ttk.Label(self, text= "Status: ")
-        
         self.lbl_status.grid()
 
         def valida (plan_padrao: str, plan_candidata: str, relatorio: str) -> bool:
             return plan_padrao != "" and plan_candidata != "" and relatorio != ""
-
-        def executa(status: ttk.Label) -> None:
-            status.config(text="Status: " + "Modificado!")
-            status.config(foreground='blue')
 
 
 
